0e91ef84f864241a7fd9c153a71dd114548a18a4
Russell Schmidt <russell.schmidt@me.com>
2018-08-04 11:05:43 -0400

    finished top nav aesthetics

--- src/routes/employeeDashboard/index.js
+++ src/routes/employeeDashboard/index.js
@@ -10,23 +10,23 @@ const EmployeeDashboard = () => (
 			<div class={style.employeeDashboard_logo}>
 				<HeaderEE />
 			</div>
-
-			<div class={style.employeeDashboard_linkContainer}>
-				<p class={style.employeeDashboard_link}>Dashboard</p>
-				<p class={style.employeeDashboard_link}>My Profile</p>
-				<p class={style.employeeDashboard_link}>Settings</p>
-			</div>
-
-			<div class={style.employeeDashboard_searchContainer}>
-				<input class={style.employeeDashboard_search} placeholder="Search Events, Location, Friends, Companies" />
-			</div>
-
-			<div class={style.employeeDashboard_profile}>
-				<div class={style.employeeDashboard_profileImg}>
-					<img src="https://storage.googleapis.com/helpinghands/images/profile_avatar%402x.png" alt="profile image" />
+			<div class={style.employerDashboard_flexContainer}>
+				<div class={style.employeeDashboard_linkContainer}>
+					<p class={style.employeeDashboard_link}>Dashboard</p>
+					<p class={style.employeeDashboard_link}>My Profile</p>
+					<p class={style.employeeDashboard_link}>Settings</p>
 				</div>
-				<div class={style.employeeDashboard_profileGreeting}>
-					<p>Welcome back!</p>
+
+				<div class={style.employeeDashboard_searchProfileContainer}>
+					<div class={style.employeeDashboard_searchContainer}>
+						<img src="https://storage.googleapis.com/helpinghands/images/magnifying-glass-icon.png" alt="magnifying glass icon"/>
+						<input class={style.employeeDashboard_search} placeholder="Search Events, Location, Friends, Companies" />
+					</div>
+
+					<div class={style.employeeDashboard_profileContainer}>
+						<img class={style.employeeDashboard_profileContainer_img} src="https://storage.googleapis.com/helpinghands/images/profile_avatar%402x.png" alt="your profile thumbnail" />
+						<p class={style.employeeDashboard_profileContainer_p}>Welcome Back!</p>
+					</div>
 				</div>
 			</div>
 		</div>
