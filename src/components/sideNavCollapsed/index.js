b2829639b25346c678fc977a44323bf4618cad22
Russell Schmidt <russell.schmidt@me.com>
2018-08-04 13:18:58 -0400

    added collapsed side nav

---
+++ src/components/sideNavCollapsed/index.js
@@ -0,0 +1,31 @@
+import { h, Component } from 'preact';
+import { Link } from 'preact-router/match';
+import style from './style';
+
+export default class sideNavCollapsed extends Component {
+	handleSidebarChange = () => {
+		this.props.onSelectSidebar();
+	}
+
+	render() {
+		return (
+			<div class={style.employeeDashboard_sidenav}>
+				<div class={style.employeeDashboard_iconContainer}>
+					<button onClick={this.handleSidebarChange}>
+						<img class={style.employeeDashboard_icon} src="https://storage.googleapis.com/helpinghands/images/dots-icon%402x.png" alt="collapse dots icon" />
+					</button>
+					<img class={style.employeeDashboard_icon} src="https://storage.googleapis.com/helpinghands/images/home-icon%402x.png" alt="active home icon" />
+					<img class={style.employeeDashboard_icon}  src="https://storage.googleapis.com/helpinghands/images/cal-icon%402x.png" alt="events calendar icon" />
+					<img class={style.employeeDashboard_icon} src="https://storage.googleapis.com/helpinghands/images/msg-icon%402x.png" alt="message icon" />
+					<img class={style.employeeDashboard_icon}  src="https://storage.googleapis.com/helpinghands/images/friend-icon%402x.png" alt="friends icon" />
+					<img class={style.employeeDashboard_icon} src="https://storage.googleapis.com/helpinghands/images/bookmark-icon%402x.png" alt="bookmark icon" />
+					<img class={style.employeeDashboard_icon} src="https://storage.googleapis.com/helpinghands/images/graph-icon%402x.png" alt="graphs icon" />
+
+					<Link href="/" class={style.employeeDashboard_icon} >
+						<img class={style.employeeDashboard_icon_exitIcon}  src="https://storage.googleapis.com/helpinghands/images/exit-icon%402x.png" alt="exit" />
+					</Link>
+				</div>
+			</div>
+		);
+	}
+}
--- src/routes/employeeDashboard/index.js
+++ src/routes/employeeDashboard/index.js
@@ -1,52 +1,50 @@
-import { h } from 'preact';
+import { h, Component } from 'preact';
 import { Link } from 'preact-router/match';
 import style from './style';
 
 import HeaderEE from '../../components/headerEE';
+import SideNavCollapsed from '../../components/sideNavCollapsed';
+import SideNavExpanded from '../../components/sideNavExpanded';
 
-const EmployeeDashboard = () => (
-	<div class={style.employeeDashboard}>
-		<div class={style.employeeDashboard_topnav}>
-			<div class={style.employeeDashboard_logo}>
-				<HeaderEE />
-			</div>
-			<div class={style.employerDashboard_flexContainer}>
-				<div class={style.employeeDashboard_linkContainer}>
-					<p class={style.employeeDashboard_link}>Dashboard</p>
-					<p class={style.employeeDashboard_link}>My Profile</p>
-					<p class={style.employeeDashboard_link}>Settings</p>
+export default class EmployeeDashboard extends Component {
+	state = {
+		sidebarOpen: false
+	};
+
+	handleViewSidebar = () => {
+		this.setState({ sidebarOpen: !this.state.sidebarOpen });
+	}
+
+	render() {
+		return (
+			<div class={style.employeeDashboard}>
+				<div class={style.employeeDashboard_topnav}>
+					<div class={style.employeeDashboard_logo}>
+						<HeaderEE />
+					</div>
+					<div class={style.employerDashboard_flexContainer}>
+						<div class={style.employeeDashboard_linkContainer}>
+							<p class={style.employeeDashboard_link}>Dashboard</p>
+							<p class={style.employeeDashboard_link}>My Profile</p>
+							<p class={style.employeeDashboard_link}>Settings</p>
+						</div>
+
+						<div class={style.employeeDashboard_searchProfileContainer}>
+							<div class={style.employeeDashboard_searchContainer}>
+								<img src="https://storage.googleapis.com/helpinghands/images/magnifying-glass-icon.png" alt="magnifying glass icon" />
+								<input class={style.employeeDashboard_search} placeholder="Search Events, Location, Friends, Companies" />
+							</div>
+
+							<div class={style.employeeDashboard_profileContainer}>
+								<img class={style.employeeDashboard_profileContainer_img} src="https://storage.googleapis.com/helpinghands/images/profile_avatar%402x.png" alt="your profile thumbnail" />
+								<p class={style.employeeDashboard_profileContainer_p}>Welcome Back!</p>
+							</div>
+						</div>
+					</div>
 				</div>
 
-				<div class={style.employeeDashboard_searchProfileContainer}>
-					<div class={style.employeeDashboard_searchContainer}>
-						<img src="https://storage.googleapis.com/helpinghands/images/magnifying-glass-icon.png" alt="magnifying glass icon"/>
-						<input class={style.employeeDashboard_search} placeholder="Search Events, Location, Friends, Companies" />
-					</div>
-
-					<div class={style.employeeDashboard_profileContainer}>
-						<img class={style.employeeDashboard_profileContainer_img} src="https://storage.googleapis.com/helpinghands/images/profile_avatar%402x.png" alt="your profile thumbnail" />
-						<p class={style.employeeDashboard_profileContainer_p}>Welcome Back!</p>
-					</div>
-				</div>
+				{this.state.sidebarOpen ? <SideNavExpanded onSelectSidebar={this.handleViewSidebar} /> : <SideNavCollapsed onSelectSidebar={this.handleViewSidebar} />}
 			</div>
-		</div>
-
-		<div class={style.employeeDashboard_sidenav}>
-			<div class={style.employeeDashboard_iconContainer}>
-				<img class={style.employeeDashboard_icon} src="https://storage.googleapis.com/helpinghands/images/dots-icon%402x.png" alt="collapse dots icon" />
-				<img class={style.employeeDashboard_icon} src="https://storage.googleapis.com/helpinghands/images/home-icon%402x.png" alt="active home icon" />
-				<img class={style.employeeDashboard_icon}  src="https://storage.googleapis.com/helpinghands/images/cal-icon%402x.png" alt="events calendar icon" />
-				<img class={style.employeeDashboard_icon} src="https://storage.googleapis.com/helpinghands/images/msg-icon%402x.png" alt="message icon" />
-				<img class={style.employeeDashboard_icon}  src="https://storage.googleapis.com/helpinghands/images/friend-icon%402x.png" alt="friends icon"/>
-				<img class={style.employeeDashboard_icon} src="https://storage.googleapis.com/helpinghands/images/bookmark-icon%402x.png" alt="bookmark icon" />
-				<img class={style.employeeDashboard_icon} src="https://storage.googleapis.com/helpinghands/images/graph-icon%402x.png" alt="graphs icon" />
-
-				<Link href="/" class={style.employeeDashboard_icon} >
-					<img class={style.employeeDashboard_icon_exitIcon}  src="https://storage.googleapis.com/helpinghands/images/exit-icon%402x.png" alt="exit" />
-				</Link>
-			</div>
-		</div>
-	</div>
-);
-
-export default EmployeeDashboard;
+		);
+	}
+}
