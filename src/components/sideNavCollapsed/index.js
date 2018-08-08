b1777809d83ecfe2783a421a669c2ee4e0e29c7b
Russell Schmidt <russell.schmidt@me.com>
2018-08-08 00:10:48 -0700

    expanded and collapsed look ok on desktop. still moving around a little

--- src/components/sideNavCollapsed/index.js
+++ src/components/sideNavCollapsed/index.js
@@ -1,28 +1,77 @@
-import { h, Component } from 'preact';
+import { h } from 'preact';
 import { Link } from 'preact-router/match';
 import style from './style';
 
-export default class sideNavCollapsed extends Component {
+const sideNavExpanded = (props) => (
+	<div class={style.employeeDashboard_exp_sidenav}>
+		<div class={style.employeeDashboard_exp_iconsContainer}>
 
-	render() {
-		return (
-			<div class={style.employeeDashboard_sidenav}>
-				<div class={style.employeeDashboard_iconContainer}>
-					<button onClick={this.props.switchSidebar}>
-						<img class={style.employeeDashboard_icon} src="https://storage.googleapis.com/helpinghands/images/dots-icon%402x.png" alt="collapse dots icon" />
+			<div id={style.icon_collapse} class={style.employeeDashboard_exp_iconContainer}>
+				<div class={style.employeeDashboard_exp_iconInnerContainer}>
+					<button onClick={props.switchSidebar}>
+						<img class={style.employeeDashboard_exp_icon} src="https://storage.googleapis.com/helpinghands/images/collapse-icon%402x.png" alt="collapse dots icon" />
 					</button>
-					<img class={style.employeeDashboard_icon} src="https://storage.googleapis.com/helpinghands/images/home-icon%402x.png" alt="active home icon" />
-					<img class={style.employeeDashboard_icon}  src="https://storage.googleapis.com/helpinghands/images/cal-icon%402x.png" alt="events calendar icon" />
-					<img class={style.employeeDashboard_icon} src="https://storage.googleapis.com/helpinghands/images/msg-icon%402x.png" alt="message icon" />
-					<img class={style.employeeDashboard_icon}  src="https://storage.googleapis.com/helpinghands/images/friend-icon%402x.png" alt="friends icon" />
-					<img class={style.employeeDashboard_icon} src="https://storage.googleapis.com/helpinghands/images/bookmark-icon%402x.png" alt="bookmark icon" />
-					<img class={style.employeeDashboard_icon} src="https://storage.googleapis.com/helpinghands/images/graph-icon%402x.png" alt="graphs icon" />
-
-					<Link href="/" class={style.employeeDashboard_icon} >
-						<img class={style.employeeDashboard_icon_exitIcon}  src="https://storage.googleapis.com/helpinghands/images/exit-icon%402x.png" alt="exit" />
-					</Link>
 				</div>
 			</div>
-		);
-	}
-}
+
+			<div id={style.icon_dashboard} class={style.employeeDashboard_exp_iconContainer}>
+				<Link>
+					<div class={style.employeeDashboard_exp_iconInnerContainer}>
+						<img class={style.employeeDashboard_exp_icon} src="https://storage.googleapis.com/helpinghands/images/home-icon%402x.png" alt="active home icon" />
+					</div>
+				</Link>
+			</div>
+
+			<div id={style.icon_events} class={style.employeeDashboard_exp_iconContainer}>
+				<Link>
+					<div class={style.employeeDashboard_exp_iconInnerContainer}>
+						<img class={style.employeeDashboard_exp_icon}  src="https://storage.googleapis.com/helpinghands/images/cal-icon%402x.png" alt="events calendar icon" />
+					</div>
+				</Link>
+			</div>
+
+			<div id={style.icon_messages} class={style.employeeDashboard_exp_iconContainer}>
+				<Link>
+					<div class={style.employeeDashboard_exp_iconInnerContainer}>
+						<img class={style.employeeDashboard_exp_icon} src="https://storage.googleapis.com/helpinghands/images/msg-icon%402x.png" alt="message icon" />
+					</div>
+				</Link>
+			</div>
+
+			<div id={style.icon_network} class={style.employeeDashboard_exp_iconContainer}>
+				<Link>
+					<div class={style.employeeDashboard_exp_iconInnerContainer}>
+						<img class={style.employeeDashboard_exp_icon}  src="https://storage.googleapis.com/helpinghands/images/friend-icon%402x.png" alt="friends icon" />
+					</div>
+				</Link>
+			</div>
+
+			<div id={style.icon_saves} class={style.employeeDashboard_exp_iconContainer}>
+				<Link>
+					<div class={style.employeeDashboard_exp_iconInnerContainer}>
+						<img class={style.employeeDashboard_exp_icon} src="https://storage.googleapis.com/helpinghands/images/bookmark-icon%402x.png" alt="bookmark icon" />
+					</div>
+				</Link>
+			</div>
+
+			<div id={style.icon_activity} class={style.employeeDashboard_exp_iconContainer}>
+				<Link>
+					<div class={style.employeeDashboard_exp_iconInnerContainer}>
+						<img class={style.employeeDashboard_exp_icon} src="https://storage.googleapis.com/helpinghands/images/graph-icon%402x.png" alt="graphs icon" />
+					</div>
+				</Link>
+			</div>
+
+			<div id={style.icon_exit} class={style.employeeDashboard_exp_iconContainer}>
+				<Link href="/" class={style.employeeDashboard_icon} >
+					<div class={style.employeeDashboard_exp_iconInnerContainer}>
+						<img class={style.employeeDashboard_exp_icon}  src="https://storage.googleapis.com/helpinghands/images/exit-icon%402x.png" alt="exit" />
+					</div>
+				</Link>
+			</div>
+
+		</div>
+	</div>
+);
+
+export default sideNavExpanded;
--- src/components/sideNavExpanded/index.js
+++ src/components/sideNavExpanded/index.js
@@ -5,42 +5,77 @@ import style from './style';
 const sideNavExpanded = (props) => (
 	<div class={style.employeeDashboard_exp_sidenav}>
 		<div class={style.employeeDashboard_exp_iconsContainer}>
+
 			<div id={style.icon_collapse} class={style.employeeDashboard_exp_iconContainer}>
-				<button onClick={props.switchSidebar}>
-					<img class={style.employeeDashboard_icon} src="https://storage.googleapis.com/helpinghands/images/collapse-icon%402x.png" alt="collapse dots icon" />
-				</button>
+				<div class={style.employeeDashboard_exp_iconInnerContainer}>
+					<button onClick={props.switchSidebar}>
+						<img class={style.employeeDashboard_exp_icon} src="https://storage.googleapis.com/helpinghands/images/collapse-icon%402x.png" alt="collapse dots icon" />
+					</button>
+				</div>
 				<p class={style.employeeDashboard_exp_p}>Collapse Navigation</p>
 			</div>
 
 			<div id={style.icon_dashboard} class={style.employeeDashboard_exp_iconContainer}>
-				<img class={style.employeeDashboard_icon} src="https://storage.googleapis.com/helpinghands/images/home-icon%402x.png" alt="active home icon" />
-				<p class={style.employeeDashboard_exp_p}>Dashboard</p>
+				<Link>
+					<div class={style.employeeDashboard_exp_iconInnerContainer}>
+						<img class={style.employeeDashboard_exp_icon} src="https://storage.googleapis.com/helpinghands/images/home-icon%402x.png" alt="active home icon" />
+					</div>
+					<p class={style.employeeDashboard_exp_p}>Dashboard</p>
+				</Link>
 			</div>
+
 			<div id={style.icon_events} class={style.employeeDashboard_exp_iconContainer}>
-				<img class={style.employeeDashboard_icon}  src="https://storage.googleapis.com/helpinghands/images/cal-icon%402x.png" alt="events calendar icon" />
-				<p class={style.employeeDashboard_exp_p}>Events</p>
+				<Link>
+					<div class={style.employeeDashboard_exp_iconInnerContainer}>
+						<img class={style.employeeDashboard_exp_icon}  src="https://storage.googleapis.com/helpinghands/images/cal-icon%402x.png" alt="events calendar icon" />
+					</div>
+					<p class={style.employeeDashboard_exp_p}>Events</p>
+				</Link>
 			</div>
+
 			<div id={style.icon_messages} class={style.employeeDashboard_exp_iconContainer}>
-				<img class={style.employeeDashboard_icon} src="https://storage.googleapis.com/helpinghands/images/msg-icon%402x.png" alt="message icon" />
-				<p class={style.employeeDashboard_exp_p}>Messages</p>
+				<Link>
+					<div class={style.employeeDashboard_exp_iconInnerContainer}>
+						<img class={style.employeeDashboard_exp_icon} src="https://storage.googleapis.com/helpinghands/images/msg-icon%402x.png" alt="message icon" />
+					</div>
+					<p class={style.employeeDashboard_exp_p}>Messages</p>
+				</Link>
 			</div>
+
 			<div id={style.icon_network} class={style.employeeDashboard_exp_iconContainer}>
-				<img class={style.employeeDashboard_icon}  src="https://storage.googleapis.com/helpinghands/images/friend-icon%402x.png" alt="friends icon" />
-				<p class={style.employeeDashboard_exp_p}>My Network</p>
+				<Link>
+					<div class={style.employeeDashboard_exp_iconInnerContainer}>
+						<img class={style.employeeDashboard_exp_icon}  src="https://storage.googleapis.com/helpinghands/images/friend-icon%402x.png" alt="friends icon" />
+					</div>
+					<p class={style.employeeDashboard_exp_p}>My Network</p>
+				</Link>
 			</div>
+
 			<div id={style.icon_saves} class={style.employeeDashboard_exp_iconContainer}>
-				<img class={style.employeeDashboard_icon} src="https://storage.googleapis.com/helpinghands/images/bookmark-icon%402x.png" alt="bookmark icon" />
-				<p class={style.employeeDashboard_exp_p}>Saved</p>
+				<Link>
+					<div class={style.employeeDashboard_exp_iconInnerContainer}>
+						<img class={style.employeeDashboard_exp_icon} src="https://storage.googleapis.com/helpinghands/images/bookmark-icon%402x.png" alt="bookmark icon" />
+					</div>
+					<p class={style.employeeDashboard_exp_p}>Saved</p>
+				</Link>
 			</div>
+
 			<div id={style.icon_activity} class={style.employeeDashboard_exp_iconContainer}>
-				<img class={style.employeeDashboard_icon} src="https://storage.googleapis.com/helpinghands/images/graph-icon%402x.png" alt="graphs icon" />
-				<p class={style.employeeDashboard_exp_p}>Activity</p>
+				<Link>
+					<div class={style.employeeDashboard_exp_iconInnerContainer}>
+						<img class={style.employeeDashboard_exp_icon} src="https://storage.googleapis.com/helpinghands/images/graph-icon%402x.png" alt="graphs icon" />
+					</div>
+					<p class={style.employeeDashboard_exp_p}>Activity</p>
+				</Link>
 			</div>
+
 			<div id={style.icon_exit} class={style.employeeDashboard_exp_iconContainer}>
 				<Link href="/" class={style.employeeDashboard_icon} >
-					<img class={style.employeeDashboard_icon_exitIcon}  src="https://storage.googleapis.com/helpinghands/images/exit-icon%402x.png" alt="exit" />
+					<div class={style.employeeDashboard_exp_iconInnerContainer}>
+						<img class={style.employeeDashboard_exp_icon}  src="https://storage.googleapis.com/helpinghands/images/exit-icon%402x.png" alt="exit" />
+					</div>
+					<p class={style.employeeDashboard_exp_p}>Exit</p>
 				</Link>
-				<p class={style.employeeDashboard_exp_p}>Exit</p>
 			</div>
 
 		</div>
