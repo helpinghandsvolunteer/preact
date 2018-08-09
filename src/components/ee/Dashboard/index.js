6a4ff4cfa5b5ce719abffde41bc61abb0e567e26
Russell Schmidt <russell.schmidt@me.com>
2018-08-09 15:24:26 -0700

    add stubout elements to dashboard

--- src/components/ee/Dashboard/index.js
+++ src/components/ee/Dashboard/index.js
@@ -4,7 +4,44 @@ import style from './style';
 
 const Dashboard = () => (
 	<div class={style.eedash_container}>
-		<p>Dashboard</p>
+    <div class={style.dash_innerContainer}>
+      <div class={style.event}>
+        <h3>Event Title</h3>
+        <p>Event description</p>
+      </div>
+
+      <div class={style.event}>
+        <h3>Event Title</h3>
+        <p>Event description</p>
+      </div>
+
+      <div class={style.event}>
+        <h3>Event Title</h3>
+        <p>Event description</p>
+      </div>
+
+      <div class={style.event}>
+        <h3>Event Title</h3>
+        <p>Event description</p>
+      </div>
+
+      <div class={style.event}>
+        <h3>Event Title</h3>
+        <p>Event description</p>
+      </div>
+
+      <div class={style.data}>
+        <h3>Dashboard data</h3>
+      </div>
+
+    </div>
+    <div class={style.dash_adContainer}>
+      <div class={style.ad_vert}>
+        <h3>Ad Copy</h3>
+        <p>Buy Snackie Smores</p>
+      </div>
+    </div>
+
 	</div>
 );
 
