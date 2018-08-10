1fb3a1bbd0c3b2a702f55e58b0d5cb49cf619d84
Russell Schmidt <russell.schmidt@me.com>
2018-08-09 17:32:31 -0700

    fix events page

--- src/components/ee/Events/index.js
+++ src/components/ee/Events/index.js
@@ -3,166 +3,21 @@ import { Link } from 'preact-router/match';
 import style from './style';
 
 const Events = () => (
-	<div class={style.eeevents_container}>
-		<div class={style.dash_innerContainer}>
-      <div class={style.event}>
-        <h3>Event Title</h3>
-        <p>Event description</p>
-      </div>
+  <div class={style.event_container}>
+    <div class={style.event_innerContainer}>
 
-      <div class={style.event}>
-        <h3>Event Title</h3>
-        <p>Event description</p>
-      </div>
 
-      <div class={style.event}>
-        <h3>Event Title</h3>
-        <p>Event description</p>
-      </div>
 
-      <div class={style.event}>
-        <h3>Event Title</h3>
-        <p>Event description</p>
-      </div>
-
-      <div class={style.event}>
-        <h3>Event Title</h3>
-        <p>Event description</p>
-      </div>
-
-            <div class={style.event}>
-        <h3>Event Title</h3>
-        <p>Event description</p>
-      </div>
-
-      <div class={style.event}>
-        <h3>Event Title</h3>
-        <p>Event description</p>
-      </div>
-
-      <div class={style.event}>
-        <h3>Event Title</h3>
-        <p>Event description</p>
-      </div>
-
-      <div class={style.event}>
-        <h3>Event Title</h3>
-        <p>Event description</p>
-      </div>
-
-      <div class={style.event}>
-        <h3>Event Title</h3>
-        <p>Event description</p>
-      </div>
-
-            <div class={style.event}>
-        <h3>Event Title</h3>
-        <p>Event description</p>
-      </div>
-
-      <div class={style.event}>
-        <h3>Event Title</h3>
-        <p>Event description</p>
-      </div>
-
-      <div class={style.event}>
-        <h3>Event Title</h3>
-        <p>Event description</p>
-      </div>
-
-      <div class={style.event}>
-        <h3>Event Title</h3>
-        <p>Event description</p>
-      </div>
-
-      <div class={style.event}>
-        <h3>Event Title</h3>
-        <p>Event description</p>
-      </div>
-
-            <div class={style.event}>
-        <h3>Event Title</h3>
-        <p>Event description</p>
-      </div>
-
-      <div class={style.event}>
-        <h3>Event Title</h3>
-        <p>Event description</p>
-      </div>
-
-      <div class={style.event}>
-        <h3>Event Title</h3>
-        <p>Event description</p>
-      </div>
-
-      <div class={style.event}>
-        <h3>Event Title</h3>
-        <p>Event description</p>
-      </div>
-
-      <div class={style.event}>
-        <h3>Event Title</h3>
-        <p>Event description</p>
-      </div>
-
-            <div class={style.event}>
-        <h3>Event Title</h3>
-        <p>Event description</p>
-      </div>
-
-      <div class={style.event}>
-        <h3>Event Title</h3>
-        <p>Event description</p>
-      </div>
-
-      <div class={style.event}>
-        <h3>Event Title</h3>
-        <p>Event description</p>
-      </div>
-
-      <div class={style.event}>
-        <h3>Event Title</h3>
-        <p>Event description</p>
-      </div>
-
-      <div class={style.event}>
-        <h3>Event Title</h3>
-        <p>Event description</p>
-      </div>
-
-      <div class={style.event}>
-        <h3>Event Title</h3>
-        <p>Event description</p>
-      </div>
-
-      <div class={style.event}>
-        <h3>Event Title</h3>
-        <p>Event description</p>
-      </div>
-
-      <div class={style.event}>
-        <h3>Event Title</h3>
-        <p>Event description</p>
-      </div>
-
-      <div class={style.event}>
-        <h3>Event Title</h3>
-        <p>Event description</p>
-      </div>
-
-      <div class={style.event}>
-        <h3>Event Title</h3>
-        <p>Event description</p>
-      </div>
     </div>
 
-    <div class={style.dash_adContainer}>
+    <div class={style.ad_container}>
       <div class={style.ad_vert}>
         <h3>Ad Copy</h3>
         <p>Buy Snackie Smores</p>
       </div>
     </div>
-	</div>
+
+  </div>
 );
 
 export default Events;
