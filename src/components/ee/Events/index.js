a00f0aca6bad10b624817c59c312186d55cea96e
Russell Schmidt <russell.schmidt@me.com>
2018-08-14 10:31:45 -0700

    change to ads copy

--- src/components/ee/Events/index.js
+++ src/components/ee/Events/index.js
@@ -3,21 +3,17 @@ import { Link } from 'preact-router/match';
 import style from './style';
 
 const Events = () => (
-  <div class={style.event_container}>
-    <div class={style.event_innerContainer}>
+	<div class={style.event_container}>
+		<div class={style.event_innerContainer} />
 
+		<div class={style.ad_container}>
+			<div class={style.ad_vert}>
+				<h3>Ad Copy</h3>
+				<p>Buy Snackie Smores</p>
+			</div>
+		</div>
 
-
-    </div>
-
-    <div class={style.ad_container}>
-      <div class={style.ad_vert}>
-        <h3>Ad Copy</h3>
-        <p>Buy Snackie Smores</p>
-      </div>
-    </div>
-
-  </div>
+	</div>
 );
 
 export default Events;
